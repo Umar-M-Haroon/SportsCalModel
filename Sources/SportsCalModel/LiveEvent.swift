b08a24dfa42fd510e9eda59660f48e83c626e58c
Umar Haroon <umar.m.haroon@gmail.com>
2022-10-24 19:48:19 -0600

    add memberwise init

--- Sources/SportsCalModel/LiveEvent.swift
+++ Sources/SportsCalModel/LiveEvent.swift
@@ -16,6 +16,32 @@ public struct LiveEvent: Codable {
 
 // MARK: - Event
 public struct Game: Codable, Identifiable {
+    internal init(idLiveScore: String? = nil, idEvent: String? = nil, strSport: String? = nil, idLeague: String? = nil, strLeague: String? = nil, idHomeTeam: String? = nil, idAwayTeam: String? = nil, strHomeTeam: String, strAwayTeam: String, strHomeTeamBadge: String? = nil, strAwayTeamBadge: String? = nil, intHomeScore: String? = nil, intAwayScore: String? = nil, strPlayer: String?? = nil, idPlayer: String?? = nil, intEventScore: String?? = nil, intEventScoreTotal: String?? = nil, strStatus: String? = nil, strProgress: String? = nil, strEventTime: String? = nil, dateEvent: String? = nil, updated: String? = nil, strTimestamp: String? = nil) {
+        self.idLiveScore = idLiveScore
+        self.idEvent = idEvent
+        self.strSport = strSport
+        self.idLeague = idLeague
+        self.strLeague = strLeague
+        self.idHomeTeam = idHomeTeam
+        self.idAwayTeam = idAwayTeam
+        self.strHomeTeam = strHomeTeam
+        self.strAwayTeam = strAwayTeam
+        self.strHomeTeamBadge = strHomeTeamBadge
+        self.strAwayTeamBadge = strAwayTeamBadge
+        self.intHomeScore = intHomeScore
+        self.intAwayScore = intAwayScore
+        self.strPlayer = strPlayer
+        self.idPlayer = idPlayer
+        self.intEventScore = intEventScore
+        self.intEventScoreTotal = intEventScoreTotal
+        self.strStatus = strStatus
+        self.strProgress = strProgress
+        self.strEventTime = strEventTime
+        self.dateEvent = dateEvent
+        self.updated = updated
+        self.strTimestamp = strTimestamp
+    }
+    
     public var id: String {
         idEvent ?? UUID().uuidString
     }
