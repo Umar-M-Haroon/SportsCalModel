5a58d47a2d7d3143488fd0beb1901122e9015010
Umar Haroon <umar.m.haroon@gmail.com>
2022-10-24 21:11:14 -0600

    fix types and remove unnecessary var

--- Sources/SportsCalModel/Team.swift
+++ Sources/SportsCalModel/Team.swift
@@ -3,16 +3,14 @@ public struct Teams: Codable {
     public var teams: [Team]
 }
 public struct Team: Codable {
-    public let idTeam: String
-    public let idAPIfootball: String
-    public let strTeam: String
-    public let strTeamShort: String
-    public let strAlternate: String
-    public let strTeamBadge: String
+    public let idTeam: String?
+    public let strTeam: String?
+    public let strTeamShort: String?
+    public let strAlternate: String?
+    public let strTeamBadge: String?
     
     enum CodingKeys: String, CodingKey {
         case idTeam = "idTeam"
-        case idAPIfootball = "idAPIfootball"
         case strTeam = "strTeam"
         case strTeamShort = "strTeamShort"
         case strAlternate = "strAlternate"
